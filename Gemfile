d8faad03e44987d5dce42e55035af67f8d22061c
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-13 12:48:19 +0300

    Add morse code

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+group :development, :test do
+  gem 'rubocop', '>= 1.0', '< 2.0'
+end
+# gem "rails"
---
+++ app.rb
@@ -0,0 +1,40 @@
+@alphabets = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(string)
+  @alphabets[string]
+end
+
+def decode_word(word)
+  word.split(/ /).map { |letter| decode_morse letter }.join
+end
+
+def decode_sentence(sentence)
+  sentence.split(/  /).map { |word| decode_word word }.join(' ')
+end
